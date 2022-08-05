4a449ab23f1e25ebe1748b09087dd2fa47613a1e
Support <Support@DESKTOP-16NLN76>
2022-08-05 11:43:49 +0100

    Add project files.

---
+++ ToyotaCorolla/Corolla2017.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyotaCorolla
+{
+    internal interface ICorollaProductionRules
+    {
+        public string carColor {get; set; } 
+        public string carName {get; set; }
+        public string carEngine { get; set; }
+        public bool carTyre(string carName);  
+    }
+}
---
+++ ToyotaCorolla/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ToyotaCorolla
+{
+    internal class Corolla : ICorollaProductionRules
+    {
+        public string carColor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string carName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string carEngine { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+       
+        static void Main(string[] args)
+        {
+            Corolla corolla = new Corolla();
+
+            Console.WriteLine("You have a car tyre");
+            Console.ReadLine();
+        }
+
+        public bool carTyre(string carName)
+            
+        {
+            return  true;
+        }
+    }
+}
